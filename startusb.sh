f1d0b9d1b884ced94216d675eaa78be99695872a
Karl Koscher <github@degdeg.com>
2021-05-09 23:00:14 -0700

    Add script to load USBIP modules

---
+++ startusb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo modprobe usbcore
+sudo modprobe usb-common
+sudo modprobe hid-generic
+sudo modprobe hid
+sudo modprobe usbnet
+sudo modprobe cdc_ether
+sudo modprobe usbserial
+sudo modprobe usb-storage
+sudo modprobe cdc-acm
+sudo modprobe ftdi_sio
+sudo modprobe usbip-core
+sudo modprobe vhci-hcd
+echo $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
